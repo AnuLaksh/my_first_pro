8806c2e35c6043995bba4baa67029eba84462702
AnuLaksh <32740051+AnuLaksh@users.noreply.github.com>
2017-10-15 15:05:22 +0530

    Update adidas.py
    Added cron job for accessing the website periodically

--- adidas.py
+++ adidas.py
@@ -1,5 +1,13 @@
 import csv
 import requests
+from crontab import CronTab
+my_cron=CronTab (url='https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=adidas+shoes')
+job=my_cron.new(command=python/home/anu/date.py)
+for job in my_cron:
+  job.day.every(1)
+  my_cron.write()
+  print job
+  print job.frequency_per_day()
 from BeautifulSoup import BeautifulSoup
 url='https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=adidas+shoes'
 response=requests.get(url)
@@ -18,7 +26,7 @@ for name in item.find('in'):
         list_of_prices.append(text)
       list_of_discriptions.append(list_of_prices)
     list_of_images.append(list_of_descriptions)
-  list_of_names.append(list_of_images)
+  list_of_names.append(list_of_images)  
 outfile=open("./inmates.cv","wb")
 writer=csv.writer(outfile)
 writer=writenames(list_of_names)
